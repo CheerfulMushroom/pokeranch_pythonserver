7151d6af862e4ad7a55b2709e371e0d52c985f68
Alex <fakeacc666@bk.ru>
2019-12-05 20:00:03 +0300

    fixed server structure

--- pokeranch_server/server_service.py
+++ pokeranch_server/server_service.py
@@ -1,6 +1,23 @@
 from aiohttp import web, WSCloseCode
-import asyncio
 import json
+
+
+@web.middleware
+async def json_check(request, handler):
+    try:
+        await request.json()
+    except Exception as e:
+        error_message = f'Couldn\'t parse json: {e}'
+        print(error_message)
+        return web.Response(body=json.dumps({'error_message': error_message}), status=400)
+    return await handler(request)
+
+
+@web.middleware
+async def log_host(request, handler):
+    addr = request.host
+    print(f"Connected: {addr}")
+    return await handler(request)
 
 
 class Server:
@@ -13,40 +30,27 @@ class Server:
             await ws.close(code=WSCloseCode.GOING_AWAY,
                            message='Server shutdown')
 
-    @staticmethod
-    async def listen(request: web.Request):
-        addr = request.host
-        print(f"Connected: {addr}")
-        try:
-            data = await request.json()
-        except Exception as e:
-            error_message = f'Couldn\'t parse json: {e}'
-            print(error_message)
-            return web.Response(body=json.dumps({'error_message': error_message}), status=400)
-
-        if 'action' not in data:
-            error_message = f'No action found'
-            print(error_message)
-            return web.Response(body=json.dumps({'error_message': error_message}), status=400)
-
-        action = data['action']
-
-        if action == 'authorize':
-            pass
-        elif action == 'save':
-            pass
-        elif action == 'get_info':
-            pass
-        else:
-            error_message = f'No such action: {action}'
-            print(error_message)
-            return web.Response(body=json.dumps({'error_message': error_message}), status=400)
-
     def start(self):
-        app = web.Application()
-        app.router.add_route('GET', '/', self.listen)
-        app.router.add_route('POST', '/', self.listen)
+        app = web.Application(middlewares=[log_host, json_check])
+        app.router.add_route('POST', '/auth', Handler.auth)
+        app.router.add_route('GET', '/get_profile', Handler.get_profile)
+        app.router.add_route('POST', '/save', Handler.save)
 
         app.on_shutdown.append(self.on_shutdown)
 
         web.run_app(app)
+
+
+class Handler:
+    @staticmethod
+    async def auth(request: web.Request):
+        return web.Response(body='auth')
+
+    @staticmethod
+    async def save(request: web.Request):
+        return web.Response(body='save')
+
+    @staticmethod
+    async def get_profile(request: web.Request):
+        return web.Response(body='get_profile')
+
